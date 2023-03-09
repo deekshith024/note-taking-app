28e0b214d8d374ad3cd3690d8b74c53c4e9ebadb
deekshith024 <105255904+deekshith024@users.noreply.github.com>
2023-03-09 16:24:33 -0500

    commit run showing notes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Routes from './routes'// import the Routes component from the routes.js file
 
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+
+  
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes /> {/* use the Routes component to render your app's routes */}
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Router>
+        <Navbar></Navbar>
+       
+        <div className="container">
+        <Routes>
+            <Route path="/" element={<Navbar />} />
+           
+        </Routes>
+        </div>
+        
+      </Router>
+    </div>
   );
 }
-
+  
 export default App;
 
 
